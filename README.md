3affa7f3e9dddf3f10eeed22c871e9c5964d8ac4
Ivan Starikov <ivan.starikov@flatstack.com>
2021-12-01 22:22:58 +0300

    Add Card component

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-# Flashcards app 
+# Flashcards app
+
+## Project structrure 
+
+./src/components - for reusable components
+./src/pages - for general pages
+
+## Colors 
+- gray: #DADADA
--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Button from './components/shared/Button';
+
+import Card from './components/shared/Card/Card';
 
 function App() {
   return (
     <>
       <h1>Flashcards</h1>
-      <Button />
+      <Card />
     </>
 
   );
